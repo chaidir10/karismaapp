efc347d05639ad633cb752ef48618be0d6fce08d
Chaidirrahman <chaidirrahman72@gmail.com>
2025-10-09 10:29:15 +0800

    perbaikan logika jumlah jam kerja

--- app/Http/Controllers/Admin/LaporanController.php
+++ app/Http/Controllers/Admin/LaporanController.php
@@ -71,7 +71,16 @@ class LaporanController extends Controller
                     $jamMasukObj  = Carbon::createFromFormat('H:i', Carbon::parse($masuk->jam)->format('H:i'));
                     $jamPulangObj = Carbon::createFromFormat('H:i', Carbon::parse($pulang->jam)->format('H:i'));
 
-                    $jamKerja = $jamMasukObj->diffInMinutes($jamPulangObj);
+                    // Jika masuk sebelum 07:30, hitung mulai dari 07:30 saja
+                    if ($jamMasukObj->lt($jamMasukDefault)) {
+                        $jamMulaiKerja = $jamMasukDefault->copy();
+                    } else {
+                        $jamMulaiKerja = $jamMasukObj->copy();
+                    }
+
+                    // Hitung total jam kerja dari waktu mulai kerja (min 07:30) hingga jam pulang
+                    $jamKerja = $jamMulaiKerja->diffInMinutes($jamPulangObj);
+
 
                     if ($isWeekend) {
                         // Akhir pekan = semua jam dianggap lembur
