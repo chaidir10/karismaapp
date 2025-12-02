abebb98f1a2c40c27ebec93acaea7375b471aff8
Chaidirrahman <chaidirrahman72@gmail.com>
2025-12-02 08:58:13 +0800

    tambah pengajuan presensi jam masuk dan pulang

---
+++ database/migrations/2025_12_02_085649_add_jam_masuk_jam_pulang_to_pengajuan_presensis_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::table('pengajuan_presensis', function (Blueprint $table) {
+            $table->time('jam_masuk')->nullable()->after('tanggal');
+            $table->time('jam_pulang')->nullable()->after('jam_masuk');
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('pengajuan_presensis', function (Blueprint $table) {
+            $table->dropColumn(['jam_masuk', 'jam_pulang']);
+        });
+    }
+};
