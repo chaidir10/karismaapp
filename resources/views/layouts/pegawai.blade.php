72b024cd610647d64445d512538c3e764116ce97
Chaidirrahman <chaidirrahman72@gmail.com>
2026-06-20 23:38:33 +0800

    speed process2

--- resources/views/layouts/pegawai.blade.php
+++ resources/views/layouts/pegawai.blade.php
@@ -29,6 +29,7 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
     <script src="https://cdn.tailwindcss.com" defer></script>
+    <meta name="turbo-cache-control" content="no-cache">
 
     <script>
         (function() {
